a8bb6b99507310021a285fee3c107075e601f0d5
hxtk <peter@psanders.me>
2019-05-08 00:05:59 -0400

    fix eggs

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="scubot-tags",
-    version="2.0.0.dev",
+    version="2.0.0.dev0",
     author="Scubot Team",
     description="Tagging system for Scubot",
     long_description=long_description,
@@ -15,11 +15,11 @@ setuptools.setup(
     install_requires=[
         "tinydb",
         "discord",
-        "reaction-scroll==0.0.1.dev"
+        "reaction-scroll==0.0.1.dev0"
     ],
     dependency_links=[
         "https://github.com/scubot/reaction-scroll/tarball/package#egg"
-        "=reaction-scroll-0.0.1.dev",
+        "=reaction-scroll-0.0.1.dev0",
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
