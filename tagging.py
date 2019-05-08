bd36f8863c94fadaa2463dc289be188f2d31da51
hxtk <peter@psanders.me>
2019-05-07 22:43:59 -0400

    Fix dependencies

--- tagging.py
+++ tagging.py
@@ -1,8 +1,9 @@
-import discord
 from discord.ext import commands
 from discord.utils import find
 from tinydb import TinyDB, Query
-import modules.reactionscroll as rs
+
+import reactionscroll as rs
+
 
 class TaggingScrollable(rs.Scrollable):
     async def preprocess(self, bot, db):
